9be8de5d936e7632b0a387591868d29a6c873450
riccardocecere <ric.cecere1@stud.uniroma3.it>
2020-03-17 18:26:46 +0100

    First stable version implemented

--- src/main/java/io/riccardocecere/coronavirustracker/models/LocationStats.java
+++ src/main/java/io/riccardocecere/coronavirustracker/models/LocationStats.java
@@ -4,6 +4,7 @@ public class LocationStats {
     private String state;
     private String country;
     private int latestTotalCases;
+    private int diffFromPrevDay;
 
     public String getState() {
         return state;
@@ -25,8 +26,16 @@ public class LocationStats {
         return latestTotalCases;
     }
 
-    public void setLatestTotalCases(int laletsTotalCases) {
+    public void setLatestTotalCases(int latestTotalCases) {
         this.latestTotalCases = latestTotalCases;
+    }
+
+    public int getDiffFromPrevDay() {
+        return diffFromPrevDay;
+    }
+
+    public void setDiffFromPrevDay(int diffFromPrevDay) {
+        this.diffFromPrevDay = diffFromPrevDay;
     }
 
     @Override
--- src/main/java/io/riccardocecere/coronavirustracker/services/CoronaVirusDataService.java
+++ src/main/java/io/riccardocecere/coronavirustracker/services/CoronaVirusDataService.java
@@ -40,10 +40,17 @@ public class CoronaVirusDataService {
             LocationStats locationStat = new LocationStats();
             locationStat.setState(record.get("Province/State"));
             locationStat.setCountry(record.get("Country/Region"));
-            locationStat.setLatestTotalCases(Integer.parseInt(record.get(record.size()-1)));
-            System.out.println(locationStat);
+            int latestCases = Integer.parseInt(record.get(record.size()-1));
+            int prevDayCases = Integer.parseInt(record.get(record.size()-2));
+            locationStat.setLatestTotalCases(latestCases);
+            locationStat.setDiffFromPrevDay(latestCases - prevDayCases);
+
             newStats.add(locationStat);
         }
         this.allStats = newStats;
+    }
+
+    public List<LocationStats> getAllStats() {
+        return allStats;
     }
 }
